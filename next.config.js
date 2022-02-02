51ee271e8613861f6bf1f7f3199eb2c8e75b11e9
arthurfiorette <arthur.fiorette@gmail.com>
2022-02-02 13:50:53 -0300

    perf: moved redirects to `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,9 +1,33 @@
+/* eslint-disable */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   swcMinify: true,
-  images: {
-    domains: ['avatars.githubusercontent.com', 'avatars.dicebear.com']
+  images: { domains: ['avatars.githubusercontent.com', 'avatars.dicebear.com'] },
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async redirects() {
+    const { REDIRECT_MAP } = process.env;
+
+    if (!REDIRECT_MAP) {
+      return [];
+    }
+
+    /** @type {import('next/dist/lib/load-custom-routes').Redirect[]} */
+    const redirects = [];
+
+    for (const redirect of REDIRECT_MAP.split(',')) {
+      const [from, to] = redirect.split('=');
+      redirects.push({
+        source: `/r/${from}`,
+        destination: to,
+        statusCode: 307 // temporary redirect
+      });
+    }
+
+    return redirects;
   }
 };
 
