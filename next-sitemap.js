a792de3aed7457cde0dcbd3fd9aa9d8f730e76f7
arthurfiorette <arthur.fiorette@gmail.com>
2022-03-19 16:16:49 -0300

    refactor: better sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,69 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { accessSync } = require('fs');
+const { lstat } = require('fs/promises');
+const path = require('path');
+
+const POSTS_REGEX = /^\/posts\/(.+)$/gm;
+
+/** @param {string} url */
+function findFile(url) {
+  const isPostUrl = url.match(POSTS_REGEX);
+
+  // Posts
+  if (isPostUrl) {
+    return path.resolve(__dirname, '.' + isPostUrl[0] + '.md');
+  }
+
+  const pagesPath = path.resolve(__dirname, './pages');
+
+  let file = path.resolve(pagesPath, './' + (url === '/' ? 'index' : url) + '.tsx');
+
+  try {
+    accessSync(file);
+
+    return file;
+  } catch {
+    // ignore
+  }
+
+  try {
+    file = path.resolve(pagesPath, './' + url, 'index.tsx');
+
+    accessSync(file);
+
+    return file;
+  } catch {
+    // ignore
+  }
+
+  throw new Error(`Could not find file for url: ${url}`);
+}
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://arthur.plce/',
+  siteUrl: 'https://arthur.place/',
+
   generateRobotsTxt: true,
+
   exclude: ['/404', '/500'],
-  trailingSlash: false
+
+  trailingSlash: false,
+
+  transform: async (_, loc) => {
+    console.log(loc);
+
+    const urlPath = findFile(loc);
+    const { atime } = await lstat(urlPath);
+
+    return {
+      loc,
+
+      // The last time the page had it's content changed
+      lastmod: atime.toISOString(),
+
+      // 1 For the index, 0.8 for all posts and 0.5 for everything else
+      priority: loc === '/' ? 1 : loc.match(POSTS_REGEX) ? 0.8 : 0.5
+    };
+  }
 };
