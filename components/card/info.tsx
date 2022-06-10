dc45bc8721f89fab1e7563b2743849a090c28481
Arthur Fiorette <arthur.fiorette@gmail.com>
2022-06-09 21:33:43 -0300

    Push to prod (#9)
    * docs: updated readme
    
    * docs: updated readme
    
    * ci: added code ci
    
    * chore: fixed build
    
    * chore: fixed again
    
    * chore: fixed ci
    
    * posts: fixed code spaces
    
    * posts: maths
    
    * feat: code line highlight
    
    * ui: replaced emoji by #
    
    * Update FUNDING.yml
    
    * feat: updated resume and fixed header
    
    * chore: dependabot
    
    * ui: slash
    
    * style: formatted code

--- components/card/info.tsx
+++ components/card/info.tsx
@@ -2,16 +2,18 @@ import { H } from 'components/h';
 import styles from 'styles/components/card/info.module.scss';
 
 export type InfoCardProps = {
-  title: string;
+  title?: string;
   children: React.ReactNode;
 };
 
 export const InfoCard = ({ title, children }: InfoCardProps) => {
   return (
     <div>
-      <H type="h4" id={title} className={styles.title}>
-        {title}
-      </H>
+      {title && (
+        <H type="h4" id={title} className={styles.title}>
+          {title}
+        </H>
+      )}
       <div className={styles.content}>{children}</div>
     </div>
   );
