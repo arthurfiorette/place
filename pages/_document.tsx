6f3ffc54231ce6e72f7961f8e5569140fa4387af
arthurfiorette <arthur.fiorette@gmail.com>
2022-06-25 16:37:00 -0300

    feat: reduced the Merriweather font size range

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,7 +21,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
 
         <link
-          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
       </Head>
