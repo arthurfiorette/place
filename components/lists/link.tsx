5dc3dc1407cb1af85d119677cc4262bdc26bbc6c
arthurfiorette <arthur.fiorette@gmail.com>
2022-06-25 17:39:38 -0300

    feat: updated resume

--- components/lists/link.tsx
+++ components/lists/link.tsx
@@ -1,13 +1,20 @@
-export const LinkList = ({ links, ordered = false }: LinkListProps) => {
+export const LinkList = ({ links, ordered = false, newPage }: LinkListProps) => {
   const items = Object.entries(links).map(([url, desc]) => {
     return (
       <li key={url}>
         {Array.isArray(desc) ? (
-          <a href={url} title={desc[0]}>
+          <a
+            href={url}
+            title={desc[0]}
+            rel="noreferrer"
+            target={newPage ? '_blank' : '_self'}
+          >
             {desc[1]}
           </a>
         ) : (
-          <a href={url}>{desc}</a>
+          <a href={url} rel="noreferrer" target={newPage ? '_blank' : '_self'}>
+            {desc}
+          </a>
         )}
       </li>
     );
@@ -18,6 +25,7 @@ export const LinkList = ({ links, ordered = false }: LinkListProps) => {
 
 export type LinkListProps = {
   ordered?: boolean;
+  newPage?: boolean;
   links: {
     [url: string]: string | [title: string, desc: string];
   };
